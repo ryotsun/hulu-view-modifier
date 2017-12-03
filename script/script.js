2b0b2d0a7847fa10b7e35d5371a0917105d1bd5a
ryotsun <ryo.tsugawa@gmail.com>
2017-12-03 18:20:40 +0900

    fix behaviour during mouseover

--- script/script.js
+++ script/script.js
@@ -25,10 +25,91 @@ $(document).ready(function() {
 
     function disablePopUp() {
         $(".vod-mod-series-area__tray, .vod-mod-tray__content").on('mouseover', "[class^='vod-mod-tray__']", function(e) {
+            var base_element = e.target.closest("[data-panel-for]");
+            if (base_element) {
+                var id = base_element.getAttribute('data-panel-for');
+                var element = document.querySelector(id);
+                var title_element = element.querySelector("header .vod-mod-popup-panel__program");
+
+                // 追加するノード
+                var div = document.createElement('div');
+                div.id = 'hvm-movie-clip-description';
+                div.innerHTML = title_element.innerHTML;
+
+                var prev_element = element.previousElementSibling.querySelector(".vod-mod-tray__thumbnail");
+                prev_element.appendChild(div);
+                $(div).fadeIn(0);
+
+                // サムネイルの変更
+                var thumbnail = element.previousElementSibling.querySelector("img");
+                var thumbnail_new = element.querySelectorAll(".vod-mod-popup-panel__picture img");
+                if (thumbnail_new[thumbnail_new.length - 1].src !== "https://www.happyon.jp/") {
+                    thumbnail.src = thumbnail_new[thumbnail_new.length - 1].src;
+                }
+            }
             return false;
         });
+        $(".vod-mod-series-area__tray, .vod-mod-tray__content").on('mouseout', function(e) {
+            var element = document.getElementById('hvm-movie-clip-description');
+            if (element) {
+                element.remove();
+            }
+
+            // サムネイルを元に戻す
+            var base_element = e.target.closest("[data-panel-for]");
+            if (base_element) {
+                var id = base_element.getAttribute('data-panel-for');
+                var element = document.querySelector(id);
+                var thumbnail = element.previousElementSibling.querySelector("img");
+                var thumbnail_new = element.querySelectorAll(".vod-mod-popup-panel__picture img");
+                if (thumbnail_new[0].src !== "https://www.happyon.jp/") {
+                    thumbnail.src = thumbnail_new[0].src;
+                }
+            }
+        });
+
         $(".vod-mod-content").on('mouseover', "[class^='vod-mod-tile__']", function(e) {
+            var base_element = e.target.closest("[data-panel-for]");
+            if (base_element) {
+                var id = base_element.getAttribute('data-panel-for');
+                var element = document.querySelector(id);
+                var title_element = element.querySelector("header .vod-mod-popup-panel__program");
+
+                // 追加するノード
+                var div = document.createElement('div');
+                div.id = 'hvm-movie-clip-description';
+                div.innerHTML = title_element.innerHTML;
+
+                var prev_element = element.previousElementSibling.querySelector(".vod-mod-tile__thumbnail");
+                prev_element.appendChild(div);
+                $(div).fadeIn(0);
+
+                // サムネイルの変更
+                var thumbnail = element.previousElementSibling.querySelector("img");
+                var thumbnail_new = element.querySelectorAll(".vod-mod-popup-panel__picture img");
+                if (thumbnail_new[thumbnail_new.length - 1].src !== "https://www.happyon.jp/") {
+                    thumbnail.src = thumbnail_new[thumbnail_new.length - 1].src;
+                }
+            }
             return false;
+        });
+        $(".vod-mod-content").on('mouseout', function(e) {
+            var element = document.querySelector("#hvm-movie-clip-description");
+            if (element) {
+                element.remove();
+            }
+
+            // サムネイルを元に戻す
+            var base_element = e.target.closest("[data-panel-for]");
+            if (base_element) {
+                var id = base_element.getAttribute('data-panel-for');
+                var element = document.querySelector(id);
+                var thumbnail = element.previousElementSibling.querySelector("img");
+                var thumbnail_new = element.querySelectorAll(".vod-mod-popup-panel__picture img");
+                if (thumbnail_new[0].src !== "https://www.happyon.jp/") {
+                    thumbnail.src = thumbnail_new[0].src;
+                }
+            }
         });
     }
 
